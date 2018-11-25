74970132f07ce52290f12103163d9e4dd3a3bad9
lucas1004jx <lucas1004jx@gmail.com>
2018-11-25 18:52:25 +0100

    change images path to public

--- src/js_components/carrusel.js
+++ src/js_components/carrusel.js
@@ -3,20 +3,29 @@ import {fetchData} from '../actions/fetchData';
 import {connect} from 'react-redux';
 
 let category='people';
-const imgPath=`./images/${category}/`;
+
+const imgPath=`/images/${category}/`;
  class Carrusel extends Component{
+     constructor(props){
+       super(props);
+       this.getIndex=this.getIndex.bind(this);
+     }
      componentDidMount(){
          this.props.fetchData('people');
+     }
+     getIndex(str){
+       return str.slice(-3).replace(/\//g,'').trim();
      }
  render(){
      if(this.props.data.sw_data.length<=0){return false}
      console.log(this.props.data.sw_data);
      let items=this.props.data.sw_data;
+     
      return(
          <div className='carrusel'>
           {items.map((item,index)=>
             <div className='item' key={index}>
-            <img src={`${imgPath}${index}.jpg`}/>
+            <img src={`${imgPath}${this.getIndex(item.url)}.jpg`}/>
              <h3 className='name'>{item.name}</h3>
             </div>
             )}
